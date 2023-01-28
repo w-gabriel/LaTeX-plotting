3af4374195a4ced22b27cf2609c4d8d3fb91868c
William Gabriel <35041613+w-gabriel@users.noreply.github.com>
2023-01-28 19:12:41 -0300

    Add files via upload

---
+++ latex_plot.py
@@ -0,0 +1,79 @@
+##############################################################
+# https://jwalton.info/Embed-Publication-Matplotlib-Latex/
+# 
+# Plot publication-quality figures with matplotlib and LaTeX
+# 
+# by Jack Walton
+# adapted by William G. A. Rocha
+# 
+# Date: 28.01.23
+##############################################################
+
+# LaTeX fonts
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
+
+# LaTeX font sizing
+tex_fonts = {
+    # Use LaTeX to write all text
+    "text.usetex": True,
+    "font.family": "serif",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": 8,
+    "font.size": 8,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize": 5,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8
+}
+matplotlib.pyplot.rcParams.update(tex_fonts)
+
+# set high resolution
+matplotlib.pyplot.rcParams['savefig.dpi'] = 300
+
+# set figure size
+def set_size(width, fraction=1):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float
+            Document textwidth or columnwidth in pts
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+
+    if width == 'report':
+        width_pt = 246.0
+    elif width == 'thesis':
+        width_pt = 426.79135
+    elif width == 'beamer':
+        width_pt = 307.28987
+    else:
+        width_pt = width
+
+    # Width of figure (in pts)
+    fig_width_pt = width_pt * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio
+
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
